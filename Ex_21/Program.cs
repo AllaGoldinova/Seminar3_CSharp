6d1c41c2bce98b2f615b04d00f40f5f9721f2ffc
Alla <alagoldinova@yandex.ru>
2022-08-16 22:23:51 +0300

    18_switch

---
+++ Ex_21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 2D пространстве
+
+int x1 = ReadInt("Введите X точки A: ");
+int y1 = ReadInt("Введите Y точки A: ");
+int x2 = ReadInt("Введите X точки B: ");
+int y2 = ReadInt("Введите Y точки B : ");
+
+int lengthX = x2 - x1;
+int lengthY = y2 - y1;
+double distance = Math.Sqrt(lengthX * lengthX + lengthY * lengthY);
+Console.WriteLine(distance);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
