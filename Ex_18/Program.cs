a65dd53709fd4ece948f162a00c10d857bc1898a
Alla <alagoldinova@yandex.ru>
2022-08-16 21:45:00 +0300

    18

---
+++ Ex_18/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+ int numberQuarter = ReadInt("Введите номер четверти: ");
+
+ if(numberQuarter == 1)
+ {
+    Console.WriteLine("x > 0, y > 0");
+ }
+
+ else if(numberQuarter == 2)
+ {
+    Console.WriteLine("x < 0, y > 0");
+ }
+
+ else if(numberQuarter == 3)
+ {
+    Console.WriteLine("x < 0, y < 0");
+ }
+
+ else if(numberQuarter == 4)
+ {
+    Console.WriteLine("x > 0, y < 0");
+ }
+    
+ else
+ {
+    Console.WriteLine("Четвертей всего четыре");
+ }
+
+
