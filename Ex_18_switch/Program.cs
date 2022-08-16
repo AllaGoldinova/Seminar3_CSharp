348913601200fc10b3f9c052a783d35b32ce9f13
Alla <alagoldinova@yandex.ru>
2022-08-16 22:01:18 +0300

    18_switch

---
+++ Ex_18_switch/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+int numberQuarter = ReadInt("Введите номер четверти: ");
+
+switch (numberQuarter)
+{
+    case 1:
+        Console.WriteLine("x > 0, y > 0");
+        break;
+    case 2:
+        Console.WriteLine("x < 0, y > 0");
+        break;
+    case 3:
+        Console.WriteLine("x < 0, y < 0");
+        break;
+    case 4:
+        Console.WriteLine("x > 0, y < 0");
+        break;
+    default:
+        Console.WriteLine("Четвертей всего четыре");
+
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
